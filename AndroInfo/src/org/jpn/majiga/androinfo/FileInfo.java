1bece71c4fb62fa5408750c5c019c03bb86e39ea
mudwell <mirainoeve@gmail.com>
2012-08-13 16:06:37 +0900

    シンボリックリンクに対応。

--- AndroInfo/src/org/jpn/majiga/androinfo/FileInfo.java
+++ AndroInfo/src/org/jpn/majiga/androinfo/FileInfo.java
@@ -9,27 +9,59 @@ public class FileInfo {
 	String name;
 	String dir;
 	boolean isDir;
+	private boolean isLink;
+	private String destPath;
 
 	public static FileInfo create(String lsout) {
 		String[] sp = lsout.split("\\s+");
 		FileInfo fileInfo = new FileInfo();
 		fileInfo.permission = sp[0].substring(1);
 		fileInfo.isDir = sp[0].charAt(0) == 'd';
+		fileInfo.isLink = sp[0].charAt(0) == 'l';
 		fileInfo.owner = sp[1];
 		fileInfo.group = sp[2];
-		if (!fileInfo.isDir()) {
+		if (fileInfo.isDir()) {
+			fileInfo.updateTime = sp[3] + " " + sp[4];
+			fileInfo.name = lsout.substring(lsout.indexOf(fileInfo.updateTime)
+					+ fileInfo.updateTime.length() + 1);
+		} else if (fileInfo.isLink()) {
+			fileInfo.updateTime = sp[3] + " " + sp[4];
+			String nameAndDistPath = lsout.substring(lsout
+					.indexOf(fileInfo.updateTime)
+					+ fileInfo.updateTime.length() + 1);
+			int delim = nameAndDistPath.indexOf(" -> ");
+			fileInfo.name = nameAndDistPath.substring(0, delim);
+			fileInfo.destPath = nameAndDistPath.substring(delim
+					+ " -> ".length());
+		} else {
 			fileInfo.size = Long.parseLong(sp[3]);
 			fileInfo.updateTime = sp[4] + " " + sp[5];
-		} else {
-			fileInfo.updateTime = sp[3] + " " + sp[4];
+			fileInfo.name = lsout.substring(lsout.indexOf(fileInfo.updateTime)
+					+ fileInfo.updateTime.length() + 1);
 		}
-		fileInfo.name = lsout.substring(lsout.indexOf(fileInfo.updateTime)
-				+ fileInfo.updateTime.length() + 1);
 
 		return fileInfo;
 	}
 
 	public boolean isDir() {
 		return isDir;
+	}
+
+	/**
+	 * このファイルがリンクであるかどうか返す.
+	 * 
+	 * @return リンクならばtrue、リンクでなければfalse
+	 */
+	public boolean isLink() {
+		return isLink;
+	}
+
+	/**
+	 * このリンクが参照しているファイルのパスを返す.
+	 * 
+	 * @return 参照ファイルの絶対パス。このFileInfoがリンクでない場合はnull。
+	 */
+	public String getDestPath() {
+		return destPath;
 	}
 }
